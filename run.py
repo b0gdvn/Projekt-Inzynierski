8ec56d36b30d31866b85c9dcce7c3f416cce4fdd
b0gdvn <pynchenkovbohdan@gmail.com>
2022-11-03 07:14:18 +0100

    base version created

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/accounts')
+def accounts():
+    return render_template("accounts.html")
 
 if __name__=="__main__":
     app.run(debug=True)
