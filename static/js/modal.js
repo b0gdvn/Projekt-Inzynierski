070304fbbd02ca2abefb953f6216e9c2b8abc92f
b0gdvn <pynchenkovbohdan@gmail.com>
2022-11-06 21:51:16 +0100

    modal updates

--- static/js/modal.js
+++ static/js/modal.js
@@ -20,3 +20,27 @@ window.onclick = function(event) {
         Modal.style.display = "none";
     }
 }
+
+document.getElementById("radio-income").addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        document.getElementById("tr-income").style.display = "block";
+        document.getElementById("tr-expense").style.display = "none";
+        document.getElementById("tr-transfer").style.display = "none";
+    }
+})
+
+document.getElementById("radio-expense").addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        document.getElementById("tr-income").style.display = "none";
+        document.getElementById("tr-expense").style.display = "block";
+        document.getElementById("tr-transfer").style.display = "none";
+    }
+})
+
+document.getElementById("radio-transfer").addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        document.getElementById("tr-income").style.display = "none";
+        document.getElementById("tr-expense").style.display = "none";
+        document.getElementById("tr-transfer").style.display = "block";
+    }
+})
