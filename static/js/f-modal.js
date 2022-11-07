13cee6d2e10ab25afc367feb5aa08c703997f970
b0gdvn <pynchenkovbohdan@gmail.com>
2022-11-07 18:46:46 +0100

    add tr modal / filter modal
    add transaction modal finalized,
    filter modal started

---
+++ static/js/f-modal.js
@@ -0,0 +1,46 @@
+// Get the modal
+var fModal = document.getElementById('filter-btn-modal');
+
+// Get the <span> element that closes the modal
+var Span = document.getElementsByClassName("tr-close")[0];
+
+// When the user clicks the button, open the modal 
+function filterBtnClick() {
+    fModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+Span.onclick = function() {
+    fModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == fModal) {
+        fModal.style.display = "none";
+    }
+}
+
+document.getElementById("radio-income").addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        document.getElementById("tr-income").style.display = "block";
+        document.getElementById("tr-expense").style.display = "none";
+        document.getElementById("tr-transfer").style.display = "none";
+    }
+})
+
+document.getElementById("radio-expense").addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        document.getElementById("tr-income").style.display = "none";
+        document.getElementById("tr-expense").style.display = "block";
+        document.getElementById("tr-transfer").style.display = "none";
+    }
+})
+
+document.getElementById("radio-transfer").addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+        document.getElementById("tr-income").style.display = "none";
+        document.getElementById("tr-expense").style.display = "none";
+        document.getElementById("tr-transfer").style.display = "block";
+    }
+})
